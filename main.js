7f400d97d2329d3d0209741b8de8a8e103f3fa2b
Rafael Finol <rafaelfinol88@gmail.com>
2023-05-06 15:06:38 -0400

    adds submit strokes to scorecard

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const currentHole = document.getElementById('holeSelect');
+const table = document.getElementById('table');
+const holeDisplay = document.querySelector('span#hole-display');
+const parDisplay = document.querySelector('span#par-display');
+const yardsDisplay = document.querySelector('span#yards-display');
+const strokesInput = document.querySelector('form.input-strokes');
 
 fetch('http://localhost:3000/holes')
   .then((resp) => resp.json())
@@ -13,19 +18,14 @@ const holeSelection = (holes) => {
 };
 
 const showHoleInfo = (holeObject) => {
-  const holeDisplay = document.querySelector('span#hole-display');
-  const parDisplay = document.querySelector('span#par-display');
-  const yardsDisplay = document.querySelector('span#yards-display');
   holeDisplay.textContent = holeObject.hole;
   parDisplay.textContent = holeObject.par;
   yardsDisplay.textContent = holeObject.yards;
 };
 
-const strokesInput = document.querySelector('form.input-trokes');
 strokesInput.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(e.target[0].value);
+  const hole = holeDisplay.textContent;
+  const strokes = e.target[0].value;
+  table.rows[hole].cells[1].textContent = strokes;
 });
-
-// const table = document.getElementById('table');
-// console.log(table.rows[0].cells[0]);
