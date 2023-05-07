8d61adf6b3cb14fafa1bc89c0a93054e180caf8f
Rafael Finol <rafaelfinol88@gmail.com>
2023-05-06 20:07:58 -0400

    adds score info for each posible score

--- main.js
+++ main.js
@@ -23,15 +23,44 @@ strokesInput.addEventListener('submit', (e) => {
   e.preventDefault();
   const hole = holeDisplay.textContent;
   const par = parDisplay.textContent;
-  //   console.log(par);
   const strokes = e.target[0].value;
-  //   console.log(strokes);
   table.rows[hole].cells[1].textContent = strokes;
   const scoreNumber = table.rows[hole].cells[2];
   const score = table.rows[hole].cells[3];
   if (par === strokes) {
     scoreNumber.textContent = 0;
     score.textContent = 'Par';
+  } else if (par - strokes === 1) {
+    scoreNumber.textContent = '-1';
+    score.textContent = 'Birdie';
+  } else if (par - strokes === 2) {
+    scoreNumber.textContent = '-2';
+    score.textContent = 'Eagle';
+  } else if (par - strokes === 3) {
+    scoreNumber.textContent = '-3';
+    score.textContent = 'Double Eagle';
+  } else if (par - strokes === 4) {
+    scoreNumber.textContent = '-4';
+    score.textContent = 'Triple Eagle';
+  } else if (par - strokes === -1) {
+    scoreNumber.textContent = '+1';
+    score.textContent = 'Bogey';
+  } else if (par - strokes === -2) {
+    scoreNumber.textContent = '+2';
+    score.textContent = 'Double Bogey';
+  } else if (par - strokes === -3) {
+    scoreNumber.textContent = '+3';
+    score.textContent = 'Triple Bogey';
+  } else if (par - strokes === -4) {
+    scoreNumber.textContent = '+4';
+    score.textContent = '+4';
+  } else if (par - strokes === -5) {
+    scoreNumber.textContent = '+5';
+    score.textContent = '+5';
+  } else if (par - strokes <= -6) {
+    scoreNumber.textContent = '+5';
+    score.textContent = 'Double Par';
   }
+
   document.querySelector('input.input-text').value = '';
 });
